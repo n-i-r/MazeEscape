970389afa5c7d4fddea56526a9ac260f1b03229b
Mohamed <mzking92@gmail.com>
2013-05-01 08:38:20 -0400

    Fixed maze selection bug with random maze movement and step counter reaching insane numbers.

--- MazeEscapeApp/src/mazeEscapeUtils/MazeNavigateTool.java
+++ MazeEscapeApp/src/mazeEscapeUtils/MazeNavigateTool.java
@@ -13,49 +13,47 @@ import org.jhotdraw.standard.AWTCursor;
 import org.jhotdraw.standard.SelectionTool;
 
 public class MazeNavigateTool extends SelectionTool {
-	
+
 	private MazeEscape mazeEscape;
-	
+
 	public MazeNavigateTool(MazeEscape me) {
-		super((DrawingEditor)me);
+		super((DrawingEditor) me);
 		mazeEscape = me;
 	}
-	public void mouseMove(MouseEvent evt, int x, int y)
-	{
-		DrawingView view=getActiveView();
-		
+	
+	@Override
+	public void mouseDrag(MouseEvent e, int x, int y) {
+	}
+	
+	public void mouseMove(MouseEvent evt, int x, int y) {
+		DrawingView view = getActiveView();
+
 		Handle handle = view.findHandle(x, y);
 		Figure figure = view.drawing().findFigure(x, y);
 
 		if (handle != null) {
 			view.setCursor(handle.getCursor());
-		}
-		else if (figure != null && isValidMove(figure)) {
+		} else if (figure != null && isValidMove(figure)) {
 			view.setCursor(new AWTCursor(java.awt.Cursor.HAND_CURSOR));
-		}
-		else if (figure != null && !isValidMove(figure))
-		{
-			//TODO: Change the Wait cursor to a Restricted cursor (or even Default would be better)
+		} else if (figure != null && !isValidMove(figure)) {
+			// TODO: Change the Wait cursor to a Restricted cursor (or even
+			// Default would be better)
 			view.setCursor(new AWTCursor(java.awt.Cursor.DEFAULT_CURSOR));
-		}
-		else {
+		} else {
 			view.setCursor(new AWTCursor(java.awt.Cursor.DEFAULT_CURSOR));
 		}
 	}
-	
-	private boolean isValidMove(Figure f)
-	{
-		if(mazeEscape.isFirstClick())
-		{
-			if((Figure)mazeEscape.getStartCell()==f)
+
+	private boolean isValidMove(Figure f) {
+		if (mazeEscape.isFirstClick()) {
+			if ((Figure) mazeEscape.getStartCell() == f)
 				return true;
 			else
 				return false;
-		}
-		else
-		{
-			if(f instanceof GCellArea)
-				return mazeEscape.getCurrentlySelected().isValidMove(mazeEscape.getCurrentlySelected(), (GCellArea)f);
+		} else {
+			if (f instanceof GCellArea)
+				return mazeEscape.getCurrentlySelected().isValidMove(
+						mazeEscape.getCurrentlySelected(), (GCellArea) f);
 			else
 				return false;
 		}
